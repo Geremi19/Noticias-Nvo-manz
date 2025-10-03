6d40532b1cdeb998ac48de30e3c721003f529bb4
Geremi19 <ggomez19@ucol.mx>
2025-10-03 09:50:06 -0600

    Cambios locales

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
+import React from "react";
+
+import "./Header.css";
 
 export default function Header() {
   return (
-    <header style={{ padding: "20px", backgroundColor: "#183C6F", color: "white" }}>
-      <h1>Mi Portal</h1>
-      <nav>
-        <Link to="/" style={{ marginRight: "15px", color: "white", textDecoration: "none" }}>
-          Inicio
-        </Link>
-        <Link to="/noticias" style={{ color: "white", textDecoration: "none" }}>
-          Noticias
-        </Link>
-      </nav>
-    </header>
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="/logouni.png" alt="logo" className="image-logo" />
+
+        <div className="flexCenter h-menu">
+          <Link to="/noticias" className="menu-link">Noticias</Link>
+          <Link to="/videos" className="menu-link">Videos</Link>
+        </div>
+      </div>
+    </section>
   );
 }
